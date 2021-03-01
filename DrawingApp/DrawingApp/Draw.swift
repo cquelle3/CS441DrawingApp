70b1e0c564e03f8416efdbcd724f981e5be21aca
Colin Quelle <cquelle3@binghamton.edu>
2021-03-01 10:15:33 -0500

    Created project

---
+++ DrawingApp/DrawingApp/Draw.swift
@@ -0,0 +1,24 @@
+//
+//  Draw.swift
+//  DrawingApp
+//
+//  Created by Colin Quelle on 3/1/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import UIKit
+
+class Draw: UIView {
+
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+     */
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        NSLog("Did a redraw of the draw");
+        
+            
+    }
+}
---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Colin Quelle on 3/1/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
